90e7ddce06fbda7eeedd0ed3b1b575699f0152fc
anusornW <78833104+anusornW@users.noreply.github.com>
2025-12-14 16:10:27 +0700

    Nested-if

--- lab3demo.c
+++ lab3demo.c
@@ -4,8 +4,13 @@ int main() {
 	scanf("%d",&score);
 	if (score >= 40)
 		printf("Pass \n");
-	else
-		printf("False \n");
+	else //39 38 37 ...
+		if (score < 30) {
+			printf("False :");
+			printf("%d \n", (40-score));
+		}
+		else //30 31 32 ... 39
+			printf("you can test again \n");
 	
 	printf("See you again \n");
 }
