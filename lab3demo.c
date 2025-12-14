7574813cf1283fcf92b33425e037d9f08988b1df
anusornW <78833104+anusornW@users.noreply.github.com>
2025-12-14 16:04:37 +0700

    use if , if (multiple if)

---
+++ lab3demo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	int score;
+	scanf("%d",&score);
+	if (score >= 40)
+		printf("Pass \n");
+	if (score < 40)
+		printf("False \n");
+	
+	printf("See you again \n");
+}
